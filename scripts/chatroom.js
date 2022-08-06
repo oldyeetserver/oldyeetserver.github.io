791bbf39330068b08ec7da3895a4088dde57f5e6
SireMiguel <commander.x474@gmail.com>
2022-08-06 23:25:17 +0200

    Version 0.0.7
    Bugfix

--- scripts/chatroom.js
+++ scripts/chatroom.js
@@ -1,21 +1,76 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+var rawchat = {
+  "aJw": {"name": "max", "message": "hello"}, "aJd": {"name": "mig", "message": "hi"}
+}
+var chat = JSON.stringify(rawchat)
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBcWTZtc9FlRzWaAKdw4DheGMXTiTUcCuU",
-  authDomain: "yeetsever.firebaseapp.com",
-  projectId: "yeetsever",
-  storageBucket: "yeetsever.appspot.com",
-  messagingSenderId: "880124899974",
-  appId: "1:880124899974:web:3985edba02af4fe89e1707",
-  measurementId: "G-DY1TGER0NF"
-};
+const name = generateName()
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+function randomInt(multiplicator) {
+  return Math.round(Math.random() * multiplicator);
+} 
+
+function stringify(json) {
+  return JSON.stringify(json)
+}
+
+function loadMessages() {
+const chatlog = JSON.parse(chat);
+let listedMessages = []
+let returnMessage = []
+
+for (let messageid in chatlog) {
+    let finalMessage = `${chatlog[messageid]["name"]}: ${chatlog[messageid]["message"]}<br>`;
+    listedMessages.push(messageid);
+    console.log(listedMessages);
+    returnMessage.push(finalMessage);
+}
+return `${returnMessage}`
+}
+
+
+function makeid(length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return result;
+}
+
+function generateName() {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789123456789';
+  const prefix = ["Schlaue", "Große", "Kleine", "Prägnante", "Schnelle", "Schöne", "Fliegende"];
+  const suffix = ["Möwe", "Giraffe", "Robbe", "Ameise", "Spinne", "Katze", "Amsel"];
+  
+  const random = Math.floor(Math.random() * 7);
+  return `${prefix[random]} ${suffix[random]}`;
+}
+
+function saveMessage(message) {
+
+}
+
+function sendMessage() {
+  const textfield = `${document.getElementById("textfield").value}`;
+  const badWords = ["stinker", "stfu"]
+  const contains = badWords.some(element => {
+    if (textfield.indexOf(element) !== -1) {
+      return true}; return false});
+
+  if (textfield != undefined) {
+    document.getElementById("textfield").value = "";
+
+    if (!contains) {
+      document.getElementById("messages").innerHTML = `${loadMessages()} ${name}: ${textfield}`
+    }
+    else {
+      console.log(`your message: '${textfield}' contains a bad word!`);
+}}}
+
+function onload() {
+  document.getElementById("messages").innerHTML = "loadMessages()"
+  console.log(generateName());
+}
